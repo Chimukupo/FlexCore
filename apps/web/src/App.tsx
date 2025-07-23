b77e62694e52c23eabb2862ae16b80b5dd01e04b
Chimukupo Goma <Chimukupo77@gmail.com>
2025-07-24 00:29:45 +0200

    Sample Component + Additional Deps

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [] = useState(0)
 
   return (
     <>
       <div className="flex items-top justify-center h-screen bg-gray-100 text-2xl font-semibold text-gray-800">
         <p>Hello World</p>
+      </div>
+
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
       </div>
     </>
   )
